1af07a31adc3e5c4f73295420449f16259eb4054
Ian White <ianwhite@utexas.edu>
2014-03-29 17:59:00 -0500

    Initialize github ENV variables.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
+GITHUB_CONFIG = YAML.load_file("#{::Rails.root}/config/github.yml")[::Rails.env]
+
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GH_BASIC_CLIENT_ID'], ENV['GH_BASIC_SECRET_ID']
+  provider :github, ENV['app_id'], ENV['secret']
 end
